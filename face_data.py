67eb89e4447bce5346335899171e61506109755f
Gopesh Khandelwal <gopeshkk@gmail.com>
2018-12-19 22:30:29 +0530

    resolved directory issues

--- face_data.py
+++ face_data.py
@@ -9,7 +9,7 @@ face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
 
 skip = 0
 face_data = []
-dataset_path = '../data/face_dataset/'
+dataset_path = './face_dataset/'
 
 file_name = input("Enter the name of the person: ")
 
